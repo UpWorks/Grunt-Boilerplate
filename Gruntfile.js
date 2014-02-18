534314c82f81bcbfaf6edfc9632ec24c0faac185
Michael Updegraff <michael.updegraff@gmail.com>
2014-02-17 19:47:20 -0800

    Removed sass and prefixr

--- Gruntfile.js
+++ Gruntfile.js
@@ -33,7 +33,7 @@ module.exports = function(grunt) {
     require('load-grunt-tasks')(grunt);
 
     // Default Task is basically a rebuild
-    grunt.registerTask('default', ['concat', 'uglify', 'sass', 'imagemin', 'autoprefixer', 'cssmin']);
+    grunt.registerTask('default', ['concat', 'uglify', 'imagemin', 'cssmin']);
 
     // Moved to the tasks folder:
     // grunt.registerTask('dev', ['connect', 'watch']);
